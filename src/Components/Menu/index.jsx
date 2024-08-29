47434833fd724d5fa157041f5e7424c4089099eb
Neto Russo <netoacrn1@gmail.com>
2024-08-29 18:13:36 -0300

    Subindo o menu desk e mobile

---
+++ src/Components/Menu/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Style from "./Menu.module.css";
+import logo from './assets/logo_menu.png';
+import ToggleButton from "../ToggleButton";
+import ToggleButtonMobile from "../ToggleButtonMobile";
+
+
+const Menu = ({ menu1, menu2, menu3 }) => {
+  return (
+    <div>
+      <div className={Style.menuDesktop}>
+        <div className={Style.navBar}>
+          <a href="#" >
+            <img src={logo} alt="logo" className={Style.logo} />
+          </a>
+          <div className={Style.navLinks}>
+            <a href="#" className={Style.navLink}>{menu1}</a>
+            <a href="#" className={Style.navLink}>{menu2}</a>
+            <a href="#" className={Style.navLink}>{menu3}</a>
+          </div>
+          <ToggleButton />
+        </div>
+      </div>
+      <div className={Style.navMobile}>
+        <div className={Style.bgModal} id="bgModal" onClick={() => {
+          document.getElementById("bgModal").style.display = "none";
+          document.getElementById("navTab").style.left = "-100%";
+        }} />
+        <div className={Style.btnMobile} id="btnMobile" onClick={() => {
+          document.getElementById("navTab").style.left = "0";
+          document.getElementById("bgModal").style.display = "block";
+        }} />
+        <div className={Style.navTab} id="navTab">
+          <button className={Style.closeTab} id="closeTab" onClick={() => {
+            document.getElementById("bgModal").style.display = "none";
+            document.getElementById("navTab").style.left = "-100%";
+          }}>X</button>
+          <a href="#">
+            <img src={logo} alt="logo" className={Style.logo} />
+          </a>
+          <div className={Style.mobileNavLinks}>
+            <a href="#" className={Style.mobileNavLink}>{menu1}</a>
+            <a href="#" className={Style.mobileNavLink}>{menu2}</a>
+            <a href="#" className={Style.mobileNavLink}>{menu3}</a>
+          </div>
+          <div className={Style.lngMobile}>
+            <ToggleButtonMobile />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Menu;
