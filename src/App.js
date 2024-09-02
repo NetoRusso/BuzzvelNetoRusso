c614ac45b3a7b6e0cbc2bd8022f440c976c522ea
Neto Russo <netoacrn1@gmail.com>
2024-09-02 01:08:07 -0300

    Ajustando as properties

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import './App.css';
 import Menu from './Components/Menu';
 import Header from './Components/Header';
@@ -7,27 +6,13 @@ import About from './Components/About';
 
 
 function App() {
-  const { t } = useTranslation();
 
   return (
     <div className="App" id='home'>
-      <Menu
-        menu1={t("Menu.menu1")}
-        menu2={t("Menu.menu2")}
-        menu3={t("Menu.menu3")}
-      />
-      <Header
-        text1={t("Greeting.text1")}
-        text2={t("Banner.text2")}
-        text3={t("Banner.text3")}
-        
-      />
+      <Menu />
+      <Header />
       <main>
-        <About 
-        aboutText1={t("About.aboutText1")}
-        aboutText2={t("About.aboutText2")}
-        aboutText3={t("About.aboutText3")}
-        />
+        <About />
       </main>
 
       <div className="espaco" />
