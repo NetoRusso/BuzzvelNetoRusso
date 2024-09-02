38e285b11136a767cc314dcd40645df60ab8c33b
Neto Russo <netoacrn1@gmail.com>
2024-09-02 01:08:22 -0300

    Ajustando as properties

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Style from './About.module.css';
 import photo from './assets/FotoNeto01.png';
 import Timeline from '../Timeline';
-const About = ( { aboutText1, aboutText2, aboutText3 } ) => {
+import { useTranslation } from 'react-i18next';
+const About = ( ) => {
+  const { t } = useTranslation();
   return (
     <section className={Style.about} id='about' aria-label='Sobre mim || About me'>
       <div className={Style.aboutResume}>
         <img src={photo} alt="Foto de Neto Russo || Picure of Neto Russo" className={Style.photo} />
         <div className={Style.aboutTexts}>
-          <p className={Style.aboutText}>{aboutText1}</p>
-            <p className={Style.aboutText}>{aboutText2}</p>
-            <p className={Style.aboutText}>{aboutText3}</p>
+          <p className={Style.aboutText}>{t("About.aboutText1")}</p>
+            <p className={Style.aboutText}>{t("About.aboutText2")}</p>
+            <p className={Style.aboutText}>{t("About.aboutText3")}</p>
         </div>
       </div>
       <div className={Style.detail} />
