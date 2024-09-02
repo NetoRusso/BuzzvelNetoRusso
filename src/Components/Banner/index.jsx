6ca44f8e2123dfda8e2794e58099362fce2696e3
Neto Russo <netoacrn1@gmail.com>
2024-09-02 01:08:43 -0300

    Ajustando as properties

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -3,9 +3,11 @@ import Style from './Banner.module.css';
 import Video from './assets/videoBanner3.mp4';
 import myPhoto from './assets/myPhoto.png';
 import Greeting from '../Greeting';
+import { useTranslation } from 'react-i18next';
 
 
-const Banner = ({ text1, text2, text3 }) => {
+const Banner = () => {
+  const { t } = useTranslation();
   return (
     <div className={Style.banner}>
       <div className={Style.overlay} />
@@ -16,14 +18,14 @@ const Banner = ({ text1, text2, text3 }) => {
       <div className={Style.container}>
         <img src={myPhoto} alt="myPhoto" className={Style.photo} />
         <div className={Style.text}>
-          <Greeting
-            text1={text1}
+          <Greeting 
+          glitch={t("Greeting.text1")}
           />
-          <h2 className={Style.subTitle}>{text2}</h2>
+          <h2 className={Style.subTitle}>{t("Banner.text2")}</h2>
           <h3 className={Style.position}>Dev. Front-end (React.js) || <br />
             Dev. Back-end (Java);
           </h3>
-          <p className={Style.slogan}>{text3}</p>
+          <p className={Style.slogan}>{t("Banner.text3")}</p>
 
         </div>
       </div>
