ca30b8beec81234d821286f6b74e63ace56252a4
Neto Russo <netoacrn1@gmail.com>
2024-09-05 18:11:40 -0300

    adicionando os componentes na pagina

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 
 
@@ -18,6 +19,7 @@ function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
 
       {/* <div className="espaco" /> */}
     </div>
