278ebd6ae5747c061bc9e56b9287c68d18895b9a
Neto Russo <netoacrn1@gmail.com>
2024-09-08 19:12:13 -0300

    Finalizando o componente Buzzvel

--- src/Components/Buzzvel/index.jsx
+++ src/Components/Buzzvel/index.jsx
@@ -1,28 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Style from "./buzzvel.module.css";
 import buzzvelLogo from './assets/logo-light.svg';
 import { useTranslation } from "react-i18next";
 import WhyBuzzvel from "../WhyBuzzvel";
+import PortifolioBuzzvel from "../PortifolioBuzzvel";
+
+import pause from './assets/pause.png';
+import play from './assets/play.png';
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Buzzvel = () => {
 
   const { t } = useTranslation();
 
+  const [botao, setBotao] = useState(false);
+
+
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+
+    let time;
+
+    if (!botao) {      
+      time = setTimeout(() => {
+        if (count === 1 && !botao) {
+          setCount(2);
+        } else if (count === 2 && !botao) {
+          setCount(3);
+        } else if (count === 3 && !botao) {
+          setCount(4);
+        } else if (count === 4 && !botao) {
+          setCount(5);
+        } else if (count === 5 && !botao) {
+          setCount(1);
+        }
+      }, 10000);
+    }
+    
+    return () => {
+      clearTimeout(time);
+    }
+
+  }, [count, botao])
+
   return (
     <section className={Style.buzzvel} aria-label="Seção sobre a Buzzvel || Section about Buzzvel" id="buzzvel">
+      <ScrollAnimation
+        animateIn="fadeInUp fadeIn"
+        animateOnce={true}
+      >
       <div className={Style.buzzvelDetail} />
       <div className={Style.buzzvelContainer}>
         <div className={Style.buzzvelHeader} >
           <a href="https://buzzvel.com/" target="_blank" rel="noreferrer">
             <img src={buzzvelLogo} alt="logo" className={Style.buzzvelLogo} />
           </a>
+
         </div>
-        <div className={Style.whyBuzzvel}> 
-          <h2 className={Style.whyBuzzvelTitle}>{t("Buzzvel.whyBuzzvel")}</h2>
-          <WhyBuzzvel />
+        <div className={Style.whyBuzzvel}>
+          <div className={Style.whyBuzzvelHeader} >
+            <h2 className={`${Style.whyBuzzvelTitle} ${Style.buzzvelTitle}`}>{t("Buzzvel.whyBuzzvel")}</h2>
+            <img src={botao ? play : pause} alt="player" className={Style.player} onClick={() => setBotao(!botao)} />
+          </div>
+          <WhyBuzzvel
+            count={count}
+          />
+        </div>
+        <div className={Style.buzzvelPortifolio}>
+          <h2 className={`${Style.buzzvelPortifolioTitle} ${Style.buzzvelTitle}`}>{t("Buzzvel.portfolio")}</h2>
+          <PortifolioBuzzvel />
         </div>
       </div>
       <div className={Style.buzzvelDetail2} />
+      </ScrollAnimation>
     </section>
   )
 };
